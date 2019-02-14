dfce78b7fb867ce537d15710421234f2480b913c
Drew Hess <src@drewhess.com>
2019-02-14 12:10:05 +0000

    Add overlays to default.nix.

--- default.nix
+++ default.nix
@@ -11,8 +11,15 @@ let
   lib = import ./lib { inherit pkgs; };
   haskell = import ./haskell { inherit pkgs; };
 
+  overlays = {
+    lib = lib.importDirectory ./overlays/lib;
+    haskell = lib.importDirectory ./overlays/haskell;
+    all = import ./overlays;
+  };
+
 in
 {
   inherit lib;
   inherit haskell;
+  inherit overlays;
 }
