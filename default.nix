ed5179096518f2a92d6ebaf4d2ffdae102236048
Drew Hess <src@drewhess.com>
2019-02-15 15:40:13 +0000

    Remove pkgs attrset from default.nix, it's not NUR-standard.

--- default.nix
+++ default.nix
@@ -17,11 +17,8 @@ in
 {
   inherit (self) lib;
   inherit (self) haskell;
+  inherit (self) melpaPackagesNgFor melpaPackagesNgFor';
 
   overlays.all = import ./overlays;
   modules = self.lib.pathDirectory ./modules;
-
-  pkgs = {
-    inherit (self) melpaPackagesNgFor melpaPackagesNgFor';
-  };
 }
