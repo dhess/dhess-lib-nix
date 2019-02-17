bd069208e1f5362469f909fe9eee396c8d10611f
Drew Hess <src@drewhess.com>
2019-02-17 19:03:26 +0000

    Provide overlay access to this lib's fixed nixpkgs and friends.
    Useful if you want to use the same pinned nixpkgs rev that the package
    is using.

--- overlays/lib/dhess-lib-nix.nix
+++ overlays/lib/dhess-lib-nix.nix
@@ -1,6 +1,9 @@
 self: super:
 
 let
+
+  localLibs = import ../../lib;
+
   # Provide access to the whole package, if needed.
   path = ../../.;
 
@@ -9,6 +12,10 @@ in
   lib = (super.lib or {}) // {
     dhess-lib-nix = (super.lib.dhess-lib-nix or {}) // {
       inherit path;
+
+      # Access to dhess-lib-nix's fixed nixpkgs, packages, and
+      # packages plus overlays.
+      inherit (localLibs) nixpkgs pkgs pkgsWithLocalOverlays;
     };
   };
 }
