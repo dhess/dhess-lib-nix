4ac545acb8abd11d764cd4a01223be0ea23a91d3
Drew Hess <src@drewhess.com>
2019-07-09 17:57:35 +0100

    Bump nixpkgs; don't build against nixos-unstable.

--- jobsets/default.nix
+++ jobsets/default.nix
@@ -55,7 +55,6 @@ let
 
   mainJobsets = with pkgs.lib; mapAttrs (name: settings: defaultSettings // settings) (rec {
     master = {};
-    nixos-unstable = mkNixpkgsChannels "master" "nixos-unstable";
     nixpkgs-unstable = mkNixpkgsChannels "master" "nixpkgs-unstable";
     nixpkgs = mkNixpkgs "master" "master";
   });
