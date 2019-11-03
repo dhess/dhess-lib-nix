6a6fd2fc926c662eeba6352d5fcf607601072a74
Drew Hess <src@drewhess.com>
2019-11-03 15:59:47 +0000

    Trying out niv.

--- lib/default.nix
+++ lib/default.nix
@@ -1,27 +1,8 @@
 let
 
-  # Adapted from https://github.com/input-output-hk/iohk-ops/blob/e6f1ae95cdbfdd5c213aa0b9a1ef67150febc503/lib.nix
+  sources = import ../nix/sources.nix;
 
-  fetchGithub = jsonSpec:
-  let
-    spec = builtins.fromJSON jsonSpec;
-  in
-    builtins.fetchTarball {
-      url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
-      inherit (spec) sha256;
-    };
-
-  # Fetch (from GitHub) a Nix expression (i.e., repo), as specified by
-  # its revision.
-  fixedNixSrc = pathOverride: jsonSpecFile:
-  let
-    try = builtins.tryEval (builtins.findFile builtins.nixPath pathOverride);
-  in
-    if try.success
-      then builtins.trace "Using <${pathOverride}>" try.value
-      else fetchGithub (builtins.readFile jsonSpecFile);
-
-  fixedNixpkgs = fixedNixSrc "nixpkgs_override" ./nixpkgs-src.json;
+  fixedNixpkgs = sources.nixpkgs-master;
   nixpkgs = import fixedNixpkgs;
   pkgs = nixpkgs {};
   lib = pkgs.lib;
@@ -48,7 +29,7 @@ in lib // {
   ## that import this one, and want to bootstrap before they can load
   ## the local overlays into their own package set.
 
-  inherit fixedNixSrc fixedNixpkgs;
+  inherit fixedNixpkgs;
   inherit nixpkgs pkgs;
   inherit composeOverlays composeOverlaysFromFiles;
 }
